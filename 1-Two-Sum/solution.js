cdb0f5742fc6ac9c61b2d25dfe6ea6c6aaadc595
Aditya <2adityapatel@gmail.com>
2026-06-10 23:45:37 +0530

    Add ques 1. Two Sum.

---
+++ 1-Two-Sum/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) { 
+
+    let numsHash = new Map();
+
+    for(let i=0;i<nums.length;i++){
+        let complement = target - nums[i]
+        if(numsHash.has(complement)){
+            return [numsHash.get(complement), i]
+        }
+        numsHash.set(nums[i], i)
+    }
+    return [];
+};
