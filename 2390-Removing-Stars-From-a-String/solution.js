028db46d3c9a63484dc64bba37591b8492df471f
Aditya <2adityapatel@gmail.com>
2026-06-14 18:05:57 +0530

    Add ques 2390. Removing Stars From a String.

---
+++ 2390-Removing-Stars-From-a-String/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+
+    let len = s.length;
+
+    let result = [];
+
+    for(let i=0;i<s.length;i++){
+
+        if(s[i] == '*'){
+            result.pop();
+        }
+        else result.push(s[i])
+
+    }
+
+    return result.join("");
+    
+};
